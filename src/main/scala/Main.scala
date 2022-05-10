75850645af3962f8feb45749774cb200d783c4a4
m.sabatowski <ma.sabatowski@gmail.com>
2022-05-10 09:50:32 +0200

    first commit

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,19 @@
+import org.apache.spark.sql.DataFrame
+
+object Main extends App {
+  import org.apache.spark.sql.SparkSession
+
+  val spark = SparkSession
+    .builder()
+    .appName("Spark SQL basic example")
+    .master("local[*]")
+    .getOrCreate()
+
+  if (args.length < 1) {
+    println("ERROR: brak argumentu")
+    sys.exit(-1)
+  }
+  val path = args(0)
+
+  spark.read.text(path).show
+}
